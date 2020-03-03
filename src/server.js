f242fe383494b363e41f747b89658be5ab54b60d
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2020-03-03 19:59:55 -0300

    :zap: use envs

--- src/server.js
+++ src/server.js
@@ -6,10 +6,16 @@ const fastify = require('fastify')({
 // Require external modules
 const mongoose = require('mongoose')
 
+const { DB_USER, DB_HOST, DB_PORT, DB_NAME } = process.env
+
 // Connect to DB
 mongoose
-	.connect('mongodb://root:root@database:27017/admin')
+	.connect(`mongodb://${DB_USER}:${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 	.then(() => console.log('MongoDB connected...'))
 	.catch(err => console.log(err))
 
 module.exports = fastify
+

57d0b342e2f35b8ee512084952269ea2545cfdc6
Vinicius Azevedo <viniazvd@gmail.com>
2020-03-04 13:49:23 -0300

    :wrench: fix config mongodb

--- src/services/db.js
+++ src/services/db.js
@@ -1,11 +1,11 @@
 // Require external modules
 const mongoose = require('mongoose')
 
-const { DB_USER, DB_HOST, DB_PORT, DB_NAME } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
 
 // connect to the database
 mongoose
-	.connect(`mongodb://${DB_USER}:${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+	.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })

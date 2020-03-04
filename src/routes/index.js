8352945340210dabd7404ef75d3ab4f98bfdae08
Vinicius Azevedo <viniazvd@gmail.com>
2020-03-04 15:53:43 -0300

    :sparkles: new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { getUsers, getSingleUser } from '../services/user'
+import { getUsers, getSingleUser, addUser, updateUser, deleteUser } from '../services/user'
 
 const routes = [
   {
@@ -10,6 +10,21 @@ const routes = [
     method: 'GET',
     url: '/api/users/:id',
     handler: getSingleUser
+  },
+  {
+    method: 'POST',
+    url: '/api/user',
+    handler: addUser
+  },
+  {
+    method: 'PUT',
+    url: '/api/user/:id',
+    handler: updateUser
+  },
+  {
+    method: 'DELETE',
+    url: '/api/user/:id',
+    handler: deleteUser
   }
 ]
 

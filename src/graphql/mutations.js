0521b0446a31f510432c8eaccd5c4e23abafb6bd
Vinicius Azevedo <viniazvd@gmail.com>
2020-03-04 15:54:05 -0300

    :zap: config resolver #addUser

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,11 +1,23 @@
 // Import External Dependancies
 import graphql from 'graphql'
 
+// Import https to make requests
+import https from 'https'
+
 // Import User Type
 import UserType from './types'
 
 // Destructure GraphQL functions
 const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLNonNull } = graphql
+
+// https options
+const options = {
+  hostname: process.env.BASE_URL,
+  port: 3000,
+  path: '/api/user',
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' }
+}
 
 // Define Mutations
 const Mutations = new GraphQLObjectType({
@@ -17,8 +29,16 @@ const Mutations = new GraphQLObjectType({
 				name: { type: new GraphQLNonNull(GraphQLString) },
 				email: { type: new GraphQLNonNull(GraphQLString) }
 			},
-			async resolve(_, args) {
-        // rest
+			resolve(_, args) {
+        https.request(options, resp => {
+          console.log(`statusCode: ${res.statusCode}`)
+
+          // A chunk of data has been recieved.
+          resp.on('data', data => console.log({ args, data }))
+
+          // The whole response has been received. Print out the result.
+          resp.on('end', data => console.log({ end: JSON.parse(data).explanation }))
+        })
 			}
 		},
 		editUser: {

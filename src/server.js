e0cfbd7adf0b45eeaf9c73f09191dac4a03168d9
Gui Barscevicius <guilbrs@gmail.com>
2020-03-04 12:25:02 -0300

    use es modules

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 
 const { DB_USER, DB_HOST, DB_PORT, DB_NAME } = process.env
 
+// connect to the database
 mongoose
 	.connect(`mongodb://${DB_USER}:${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
     useNewUrlParser: true,
@@ -11,5 +12,6 @@ mongoose
 	.then(() => console.log('MongoDB connected...'))
 	.catch(err => console.log(err))
 
+// export fastify instance
 export default fastify({ logger: true })
 
